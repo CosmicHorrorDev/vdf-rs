8284725a994893cc6bdc181d5fe8a391086e2a1b
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-05-22 14:52:49 -0400

    Add in tests for fuzzer crashes
    This crash isnt really unexpected from the new unquoted strings.
    Its just crashing when a string contains a "\" since its valid
    in an unquoted string, but wont be escaped properly when its
    rendered back to a quoted string

---
+++ keyvalues-parser/tests/fuzzer_crashes.rs
@@ -0,0 +1,23 @@
+use keyvalues_parser::core::Vdf;
+use pretty_assertions::assert_eq;
+
+use std::{fs, path::Path};
+
+fn read_crash_file(file_name: &str) -> String {
+    let crash_file = Path::new("tests").join("crash_outputs").join(file_name);
+    fs::read_to_string(crash_file).unwrap()
+}
+
+type BoxedResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+#[test]
+fn failure1() -> BoxedResult<()> {
+    let contents = read_crash_file("crash-1");
+
+    let parsed = Vdf::parse(&contents).unwrap();
+    let vdf_text = parsed.to_string();
+    let reparsed = Vdf::parse(&vdf_text)?;
+    assert_eq!(parsed, reparsed);
+
+    Ok(())
+}
