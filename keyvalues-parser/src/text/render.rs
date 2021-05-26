49364d7a931a3b93b44a945863d12a0d88f85504
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-05-26 14:52:50 -0400

    Rendering didnt deal with all special characters correctly

--- keyvalues-parser/src/text/render.rs
+++ keyvalues-parser/src/text/render.rs
@@ -11,6 +11,9 @@ fn write_escaped_str(f: &mut fmt::Formatter<'_>, s: &str) -> fmt::Result {
 
     for c in s.chars() {
         match c {
+            '\n' => f.write_str(r"\n"),
+            '\t' => f.write_str(r"\t"),
+            '\"' => f.write_str(r#"\""#),
             '\\' => f.write_str(r"\\"),
             reg => f.write_char(reg),
         }?
--- keyvalues-parser/tests/fuzzer_crashes.rs
+++ keyvalues-parser/tests/fuzzer_crashes.rs
@@ -3,21 +3,26 @@ use pretty_assertions::assert_eq;
 
 use std::{fs, path::Path};
 
-fn read_crash_file(file_name: &str) -> String {
-    let crash_file = Path::new("tests").join("crash_outputs").join(file_name);
-    fs::read_to_string(crash_file).unwrap()
-}
-
 type BoxedResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-#[test]
-fn fuzz_failure_1() -> BoxedResult<()> {
-    let contents = read_crash_file("crash-1");
+macro_rules! parse_fuzz_regression_test {
+    ($func_name:ident, $file_name: literal) => {
+        #[test]
+        fn $func_name() -> BoxedResult<()> {
+            let crash_file = Path::new("tests").join("crash_outputs").join($file_name);
+            let contents = fs::read_to_string(crash_file)?;
 
-    let parsed = Vdf::parse(&contents).unwrap();
-    let vdf_text = parsed.to_string();
-    let reparsed = Vdf::parse(&vdf_text)?;
-    assert_eq!(parsed, reparsed);
+            // This should be infallible unless the grammar changes in which case the test is no
+            // longer valid
+            let parsed = Vdf::parse(&contents).expect("Input has to be valid here");
+            let vdf_text = parsed.to_string();
+            let reparsed = Vdf::parse(&vdf_text)?;
+            assert_eq!(parsed, reparsed);
 
-    Ok(())
+            Ok(())
+        }
+    };
 }
+
+parse_fuzz_regression_test!(fuzzer_crash_1, "crash-1");
+parse_fuzz_regression_test!(fuzzer_crash_2, "crash-2");
