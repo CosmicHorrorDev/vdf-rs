08c613ba70cc9613e6acf33d697c41c0a74281f1
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-05-30 22:35:53 -0400

    Bump MSRV back up to 1.42.0

--- keyvalues-parser/src/tokens/mod.rs
+++ keyvalues-parser/src/tokens/mod.rs
@@ -62,24 +62,18 @@ impl<'a> TokenStream<'a> {
     }
 
     pub fn peek_is_key(&self) -> bool {
-        match self.peek() {
-            Some(Token::Key(_)) => true,
-            _ => false,
-        }
+        matches!(self.peek(), Some(Token::Key(_)))
     }
 
     pub fn peek_is_str(&self) -> bool {
-        match self.peek() {
-            Some(Token::Str(_)) => true,
-            _ => false,
-        }
+        matches!(self.peek(), Some(Token::Str(_)))
     }
 
     pub fn peek_is_value(&self) -> bool {
-        match self.peek() {
-            Some(Token::ObjBegin) | Some(Token::SeqBegin) | Some(Token::Str(_)) => true,
-            _ => false,
-        }
+        matches!(
+            self.peek(),
+            Some(Token::ObjBegin) | Some(Token::SeqBegin) | Some(Token::Str(_))
+        )
     }
 
     pub fn next_key(&mut self) -> Option<Cow<'a, str>> {
