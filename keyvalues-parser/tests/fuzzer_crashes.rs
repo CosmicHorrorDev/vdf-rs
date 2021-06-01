b714f230c3bece4d2578e841b12141c842f5c714
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-06-01 03:22:53 -0400

    Reuse common test structure

--- keyvalues-parser/tests/fuzzer_crashes.rs
+++ keyvalues-parser/tests/fuzzer_crashes.rs
@@ -5,21 +5,25 @@ use std::{fs, path::Path};
 
 type BoxedResult<T> = Result<T, Box<dyn std::error::Error>>;
 
+fn parse_fuzz_test(file_name: &str) -> BoxedResult<()> {
+    let crash_file = Path::new("tests").join("crash_outputs").join(file_name);
+    let contents = fs::read_to_string(crash_file)?;
+
+    // This should be infallible unless the grammar changes in which case the test is no longer
+    // valid
+    let parsed = Vdf::parse(&contents).expect("Input has to be valid here");
+    let vdf_text = parsed.to_string();
+    let reparsed = Vdf::parse(&vdf_text)?;
+    assert_eq!(parsed, reparsed);
+
+    Ok(())
+}
+
 macro_rules! parse_fuzz_regression_test {
-    ($func_name:ident, $file_name: literal) => {
+    ($func_name:ident, $file_name:literal) => {
         #[test]
         fn $func_name() -> BoxedResult<()> {
-            let crash_file = Path::new("tests").join("crash_outputs").join($file_name);
-            let contents = fs::read_to_string(crash_file)?;
-
-            // This should be infallible unless the grammar changes in which case the test is no
-            // longer valid
-            let parsed = Vdf::parse(&contents).expect("Input has to be valid here");
-            let vdf_text = parsed.to_string();
-            let reparsed = Vdf::parse(&vdf_text)?;
-            assert_eq!(parsed, reparsed);
-
-            Ok(())
+            parse_fuzz_test($file_name)
         }
     };
 }
--- keyvalues-serde/tests/tests.rs
+++ keyvalues-serde/tests/tests.rs
@@ -40,150 +40,6 @@ where
     Ok(())
 }
 
-macro_rules! test_ser_de {
-    ($test_val:ident, $file_name:literal) => {{
-        let vdf_text = read_asset_file($file_name)?;
-        test_vdf_deserialization(&vdf_text, &$test_val)?;
-        test_vdf_serialization(&vdf_text, &$test_val)
-    }};
-}
-
-#[test]
-fn basic_struct() -> BoxedResult<()> {
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
-    struct TestStruct {
-        field1: i32,
-        field2: String,
-    }
-
-    let val = TestStruct {
-        field1: -123,
-        field2: String::from("Sample String"),
-    };
-    test_ser_de!(val, "basic_struct.vdf")
-}
-
-#[test]
-fn basic_types() -> BoxedResult<()> {
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
-    struct BasicTypes {
-        boolean: bool,
-        character: char,
-        signed8: i8,
-        signed16: i16,
-        signed32: i32,
-        signed64: i64,
-        unsigned8: u8,
-        unsigned16: u16,
-        unsigned32: u32,
-        unsigned64: u64,
-        float32: f32,
-        float64: f64,
-    }
-
-    let val = BasicTypes {
-        boolean: false,
-        character: 'a',
-        signed8: 1,
-        signed16: 2,
-        signed32: 3,
-        signed64: 4,
-        unsigned8: 5,
-        unsigned16: 6,
-        unsigned32: 7,
-        unsigned64: 8,
-        float32: 1.0,
-        float64: 2.0,
-    };
-    test_ser_de!(val, "basic_types.vdf")
-}
-
-#[test]
-fn nested_structs() -> BoxedResult<()> {
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
-    struct OuterStruct {
-        field: String,
-        inner1: Container<String>,
-        inner2: Container<String>,
-    }
-
-    let val = OuterStruct {
-        field: String::from("Outer Value"),
-        inner1: Container::new(String::from("Inner1 Value")),
-        inner2: Container::new(String::from("Inner2 Value")),
-    };
-    test_ser_de!(val, "nested_structs.vdf")
-}
-
-#[test]
-fn newtype_struct() -> BoxedResult<()> {
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
-    struct I32Wrapper(i32);
-
-    let val = Container::new(I32Wrapper(123));
-    test_ser_de!(val, "newtype_struct.vdf")
-}
-
-#[test]
-fn unit_variant_enum() -> BoxedResult<()> {
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
-    enum SampleEnum {
-        Foo,
-        Bar,
-    }
-
-    let val = Container::new(SampleEnum::Foo);
-    test_ser_de!(val, "unit_variant_enum.vdf")
-}
-
-#[test]
-fn sequence_single() -> BoxedResult<()> {
-    let val = Container::new(vec![Container::new(String::from("Some String"))]);
-    test_ser_de!(val, "sequence_single.vdf")
-}
-
-#[test]
-fn sequence_double() -> BoxedResult<()> {
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
-    struct Inner {
-        field: String,
-    }
-
-    let val = Container::new(vec![
-        Container::new(String::from("Some String")),
-        Container::new(String::from("Another String")),
-    ]);
-    test_ser_de!(val, "sequence_double.vdf")
-}
-
-#[test]
-fn tuple() -> BoxedResult<()> {
-    let val = Container::new((true, 2, String::from("Sample Text")));
-    test_ser_de!(val, "tuple.vdf")
-}
-
-#[test]
-fn tuple_struct() -> BoxedResult<()> {
-    #[derive(Deserialize, Serialize, Debug, PartialEq)]
-    struct TupleStruct(bool, i32, String);
-
-    let val = Container::new(TupleStruct(true, 2, String::from("Sample Text")));
-    test_ser_de!(val, "tuple.vdf")
-}
-
-// TODO: it's not clear if the ordering of values is expected to stay the same in vdf. If that is
-// the case then it would be important to track down a map type that preserves insertion order. It
-// looks like something like hashlink should work out
-#[test]
-fn hashmap_nested() -> BoxedResult<()> {
-    let mut inner = HashMap::new();
-    inner.insert(0, "Foo".to_owned());
-    inner.insert(1, "Bar".to_owned());
-    inner.insert(2, "Baz".to_owned());
-    let val = Container::new(inner);
-    test_ser_de!(val, "hashmap_nested.vdf")
-}
-
 #[test]
 fn hashmap_top_level() -> BoxedResult<()> {
     let mut val = HashMap::new();
@@ -200,6 +56,7 @@ fn hashmap_top_level() -> BoxedResult<()> {
     Ok(())
 }
 
+// Deserialization throws away the top level key, so `from_str_with_key` is needed to read it
 #[test]
 fn check_deserialization_key() -> BoxedResult<()> {
     let vdf_text = read_asset_file("hashmap_top_level.vdf")?;
@@ -209,14 +66,158 @@ fn check_deserialization_key() -> BoxedResult<()> {
     Ok(())
 }
 
-#[test]
-fn option_none() -> BoxedResult<()> {
-    let val: Container<Option<String>> = Container::new(None);
-    test_ser_de!(val, "option_none.vdf")
+macro_rules! test_ser_de {
+    ($func_name:ident, $test_val:expr, $file_name:literal) => {
+        #[test]
+        fn $func_name() -> BoxedResult<()> {
+            let vdf_text = read_asset_file($file_name)?;
+            test_vdf_deserialization(&vdf_text, &$test_val)?;
+            test_vdf_serialization(&vdf_text, &$test_val)
+        }
+    };
 }
 
-#[test]
-fn option_some() -> BoxedResult<()> {
-    let val = Container::new(Some(String::from("Some value")));
-    test_ser_de!(val, "option_some.vdf")
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+struct TestStruct {
+    field1: i32,
+    field2: String,
 }
+
+test_ser_de!(
+    basic_struct,
+    TestStruct {
+        field1: -123,
+        field2: String::from("Sample String")
+    },
+    "basic_struct.vdf"
+);
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+struct BasicTypes {
+    boolean: bool,
+    character: char,
+    signed8: i8,
+    signed16: i16,
+    signed32: i32,
+    signed64: i64,
+    unsigned8: u8,
+    unsigned16: u16,
+    unsigned32: u32,
+    unsigned64: u64,
+    float32: f32,
+    float64: f64,
+}
+
+test_ser_de!(
+    basic_types,
+    BasicTypes {
+        boolean: false,
+        character: 'a',
+        signed8: 1,
+        signed16: 2,
+        signed32: 3,
+        signed64: 4,
+        unsigned8: 5,
+        unsigned16: 6,
+        unsigned32: 7,
+        unsigned64: 8,
+        float32: 1.0,
+        float64: 2.0,
+    },
+    "basic_types.vdf"
+);
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+struct OuterStruct {
+    field: String,
+    inner1: Container<String>,
+    inner2: Container<String>,
+}
+
+test_ser_de!(
+    nested_structs,
+    OuterStruct {
+        field: String::from("Outer Value"),
+        inner1: Container::new(String::from("Inner1 Value")),
+        inner2: Container::new(String::from("Inner2 Value")),
+    },
+    "nested_structs.vdf"
+);
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+struct I32Wrapper(i32);
+
+test_ser_de!(
+    newtype_struct,
+    Container::new(I32Wrapper(123)),
+    "newtype_struct.vdf"
+);
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+enum SampleEnum {
+    Foo,
+    Bar,
+}
+
+test_ser_de!(
+    unit_variant_enum,
+    Container::new(SampleEnum::Foo),
+    "unit_variant_enum.vdf"
+);
+
+test_ser_de!(
+    sequence_single,
+    Container::new(vec![Container::new(String::from("Some String"))]),
+    "sequence_single.vdf"
+);
+
+test_ser_de!(
+    sequence_double,
+    Container::new(vec![
+        Container::new(String::from("Some String")),
+        Container::new(String::from("Another String")),
+    ]),
+    "sequence_double.vdf"
+);
+
+test_ser_de!(
+    tuple,
+    Container::new((true, 2, String::from("Sample Text"))),
+    "tuple.vdf"
+);
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+struct TupleStruct(bool, i32, String);
+
+test_ser_de!(
+    tuple_struct,
+    Container::new(TupleStruct(true, 2, String::from("Sample Text"))),
+    "tuple.vdf"
+);
+
+// TODO: it's not clear if the ordering of values is expected to stay the same in vdf. If that is
+// the case then it would be important to track down a map type that preserves insertion order. It
+// looks like something like hashlink should work out
+test_ser_de!(
+    hashmap_nested,
+    {
+        let mut inner = HashMap::new();
+        inner.insert(0, "Foo".to_owned());
+        inner.insert(1, "Bar".to_owned());
+        inner.insert(2, "Baz".to_owned());
+        Container::new(inner)
+    },
+    "hashmap_nested.vdf"
+);
+
+test_ser_de!(
+    option_none,
+    Container::<Option<String>>::new(None),
+    "option_none.vdf"
+);
+
+test_ser_de!(
+    option_some,
+    Container::new(Some(String::from("Some value"))),
+    "option_some.vdf"
+);
