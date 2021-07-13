c7130a8df3fa377cbfb94cddb3a8c023a9c58962
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-07-13 19:22:28 -0400

    Fix clippy lint triggering from `thiserror` change

--- keyvalues-serde/src/error.rs
+++ keyvalues-serde/src/error.rs
@@ -1,5 +1,14 @@
 //! Contains error information for `keyvalues-serde`
 
+// This library supports an MSRV of 1.42.0 which is before the addition of
+// clippy::nonstandard_macro_braces. This lint is used within `thiserror` which in turn gets
+// expanded out here causing clippy to throw out an unknown lint warning which fails CI. Until this
+// gets resolved upstream I'm going to allow `unknown_clippy_lints` as a stopgap. Relevant:
+// https://github.com/dtolnay/thiserror/issues/140
+// https://github.com/dtolnay/thiserror/issues/141
+#![allow(renamed_and_removed_lints)]
+#![allow(clippy::unknown_clippy_lints)]
+
 use keyvalues_parser::error::Error as ParserError;
 use serde::{de, ser};
 use thiserror::Error as ThisError;
