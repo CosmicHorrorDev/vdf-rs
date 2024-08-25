f1c589aaaa641301df9691b81015321d7f057fbb
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-08-25 00:02:48 -0600

    test(parser): Add test demoing escaped char stackoverflow

---
+++ keyvalues-parser/tests/regression_tests.rs
@@ -0,0 +1,20 @@
+use std::iter;
+
+use keyvalues_parser::Vdf;
+
+// The parser could hit a stack overflow when dealing with a string containing a large amount of
+// escape sequences. This happened in the wild from a VDF file that contained a large JSON blob in
+// a string that required a lot of escapes
+#[test]
+fn issue_54() {
+    // Generates strings e.g. `lots_of_escaped(2)` gives `"" "\"\""`
+    fn lots_of_escapes(num_escaped: usize) -> String {
+        iter::once("\"\" \"")
+            .chain(iter::repeat("\\\"").take(num_escaped))
+            .chain(iter::once("\""))
+            .collect()
+    }
+
+    let vdf_text = lots_of_escapes(20_000);
+    Vdf::parse(&vdf_text).unwrap();
+}
