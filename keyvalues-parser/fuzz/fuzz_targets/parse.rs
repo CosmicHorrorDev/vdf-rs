ae642175a7ce234aa6a22a78d84548c07d8cd602
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-08-31 00:42:22 -0600

    fuzz: Fuzz through both raw and normal mode

--- keyvalues-parser/fuzz/fuzz_targets/parse.rs
+++ keyvalues-parser/fuzz/fuzz_targets/parse.rs
@@ -1,14 +1,23 @@
 #![no_main]
-use keyvalues_parser::Vdf;
+use keyvalues_parser::{error::Result, Vdf};
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        let _ = Vdf::parse(s).map(|initial_vdf| {
+fuzz_target!(|input: (bool, &str)| {
+    let (raw, text) = input;
+    if raw {
+        let _ = Vdf::parse_raw(text).map(|parsed| {
+            let mut rendered = String::new();
+            parsed.render_raw(&mut rendered).unwrap();
+            let reparsed = Vdf::parse_raw(&rendered).unwrap();
+            assert_eq!(parsed, reparsed);
+        });
+    } else {
+        let _ = Vdf::parse(text).map(|parsed| {
             // Now try round tripping to a string and back to see if it's the same
-            let unparsed = initial_vdf.to_string();
-            let reparsed_vdf = Vdf::parse(&unparsed).unwrap();
-            assert_eq!(initial_vdf, reparsed_vdf);
+            let mut rendered = String::new();
+            parsed.render(&mut rendered).unwrap();
+            let reparsed = Vdf::parse(&rendered).unwrap();
+            assert_eq!(parsed, reparsed);
         });
     }
 });
