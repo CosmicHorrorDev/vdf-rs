a313f726a9b2b7f888dee7814fc7ad1a62167a2f
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-09-17 00:14:21 -0600

    fix: avoid recursion in string parsing

--- keyvalues-parser/tests/regressions/mod.rs
+++ keyvalues-parser/tests/regressions/mod.rs
@@ -1,6 +1,22 @@
+use std::iter;
+
 use keyvalues_parser::Vdf;
 
 mod fuzzer;
+
+#[test]
+fn issue_54() {
+    // Generates strings e.g. `lots_of_escaped(2)` gives `"" "\"\""`
+    fn lots_of_escapes(num_escaped: usize) -> String {
+        iter::once("\"\" \"")
+            .chain(iter::repeat("\\\"").take(num_escaped))
+            .chain(iter::once("\""))
+            .collect()
+    }
+
+    let vdf_text = lots_of_escapes(20_000);
+    Vdf::parse(&vdf_text).unwrap();
+}
 
 #[test]
 fn raw_no_newline() {
